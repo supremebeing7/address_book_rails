fa27e677d7a8644870d2994472fa9cd012b6348b
Mark Lehman <markopolo@gmail.com>
2014-03-30 11:17:01 -0700

    build out full CRUD capability for contacts

--- app/controllers/contacts_controller.rb
+++ app/controllers/contacts_controller.rb
@@ -11,8 +11,8 @@ class ContactsController < ApplicationController
 
   def create
     @contact = Contact.new(name: params[:name],
-                              phone: params[:phone],
-                              email: params[:email])
+                           phone: params[:phone],
+                           email: params[:email])
     if @contact.save
       render('contacts/success.html.erb')
     else
@@ -23,5 +23,27 @@ class ContactsController < ApplicationController
   def show
     @contact = Contact.find(params[:id])
     render('contacts/show.html.erb')
+  end
+
+  def edit
+    @contact = Contact.find(params[:id])
+    render('contacts/edit.html.erb')
+  end
+
+  def update
+    @contact = Contact.find(params[:id])
+    if @contact.update(name: params[:name],
+                        phone: params[:phone],
+                        email: params[:email])
+      render('contacts/success.html.erb')
+    else
+      render('contacts/edit.html.erb')
+    end
+  end
+
+  def destroy
+    @contact = Contact.find(params[:id])
+    @contact.destroy
+    render('contacts/destroy.html.erb')
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,11 @@
 Wikipages::Application.routes.draw do
+  match('/', {via: :Get, to: 'contacts#index'})
   match('contacts', {via: :get, to: 'contacts#index'})
   match('contacts/new', {via: :get, to: 'contacts#new'})
   match('contacts', {via: :post, to: 'contacts#create'})
   match('contacts/:id', {via: :get, to: 'contacts#show'})
+  match('contacts/:id/edit', {via: :get, to: 'contacts#edit'})
+  match('contacts/:id', {via: [:patch, :put], to: 'contacts#update'})
+  match('contacts/:id', {via: :delete, to: 'contacts#destroy'})
 end
 
